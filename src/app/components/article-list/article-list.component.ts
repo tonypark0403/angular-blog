22e982fad0bde6e369d22b488b38fe2d843710a1
Tony Park <tonypark0403@gmail.com>
2019-10-05 22:29:49 -0400

    added article-list

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Article } from "src/app/model/article";
+import { ARTICLES } from "src/app/model/mock-articles";
 
 @Component({
-  selector: 'app-article-list',
-  templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.scss']
+  selector: "app-article-list",
+  templateUrl: "./article-list.component.html",
+  styleUrls: ["./article-list.component.scss"]
 })
 export class ArticleListComponent implements OnInit {
-
-  constructor() { }
+  articles: Article[] = [];
+  constructor() {}
 
   ngOnInit() {
-  }
+    this.articles = ARTICLES;
 
+    console.log(this.articles);
+  }
 }
